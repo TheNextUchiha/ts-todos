a16a22da81bbe16c5f4538182656e985c102a5c4
Farzan Pira <farzanpira3@gmail.com>
2025-09-14 19:14:26 +0530

    Fixed __dirname not usable directly in a ES module.

--- src/config/i18next.ts
+++ src/config/i18next.ts
@@ -1,7 +1,12 @@
 import i18next from 'i18next';
 import Backend from 'i18next-fs-backend';
 import { LanguageDetector } from 'i18next-http-middleware';
-import path from 'path';
+
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filepath);
 
 i18next
     .use(Backend)
